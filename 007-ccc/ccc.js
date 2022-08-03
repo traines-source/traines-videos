434f585d0e2afb81dc6677f04ff26b74e698abb9
Traines <git@traines.eu>
2022-08-03 21:53:09 +0000

    ccc timing, tweaks

--- 007-ccc/ccc.js
+++ 007-ccc/ccc.js
@@ -15,49 +15,52 @@ async function perm(delay, permutation) {
 }
 
 async function scene() {
-    await defer(2000);
+    await defer(7000);
     mapManager.fetchMapFile('/videos/007-ccc/greenland/out/grown_greenland.csv');
     await perm( 5000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-0');
 
-    await perm( 5000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-0_wbgt-0');
-    await perm(10000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-0_wbgt-0');
+    await perm( 7000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-0_wbgt-0');
+    await perm(25000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-0_wbgt-0');
 
-    await perm(10000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-1_sealevel-0_wbgt-0');
-    await perm( 5000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
-    await perm( 5000, '2100-1_coalplants-1_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
-    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-0_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
+    await perm(25000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-1_sealevel-0_wbgt-0');
+    await perm(20000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
+    await perm(13000, '2100-1_coalplants-1_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
+    await perm(10000, '2100-1_coalplants-1_industry-1_roadtraffic-0_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
     await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
-    await perm( 5000, '2100-0_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
+    await perm(19000, '2100-0_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-1_sealevel-0_wbgt-0');
     
     await perm( 7000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-0');
     
     await perm( 7000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-1_wbgt-0');
-    await perm( 5000, '2100-1_coalplants-1_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-1_wbgt-0');
-    await perm( 3000, '2100-1_coalplants-1_industry-1_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-1_wbgt-0');
-    await perm( 3000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-0_sealevel-1_wbgt-0');
-    await perm( 3000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-1_population-0_wildfires-0_sealevel-1_wbgt-0');
-    await perm( 5000, '2100-0_coalplants-1_industry-1_roadtraffic-1_15degrees-1_population-0_wildfires-0_sealevel-1_wbgt-0');
+    await perm(19000, '2100-1_coalplants-1_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-1_wbgt-0');
+    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-1_wbgt-0');
+    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-0_sealevel-1_wbgt-0');
+    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-1_population-0_wildfires-0_sealevel-1_wbgt-0');
+    await perm(12000, '2100-0_coalplants-1_industry-1_roadtraffic-1_15degrees-1_population-0_wildfires-0_sealevel-1_wbgt-0');
     
-    await perm( 5000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-1_wbgt-0');
+    await perm(10000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-1_wbgt-0');
 
+    await perm(24000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-0');
+    
+    await perm( 7000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1'); //3:58
+    await perm(16000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-1_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm( 6000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm( 6000, '2100-1_coalplants-1_industry-1_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm( 6000, '2100-1_coalplants-1_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm( 6000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm(21000, '2100-1_coalplants-1_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm( 4000, '2100-1_coalplants-1_industry-1_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm( 4000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm( 6000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-1_population-0_wildfires-0_sealevel-0_wbgt-1');
+    await perm(12000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-1_wildfires-0_sealevel-0_wbgt-1');
+    
     await perm(10000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-0');
-    
-    await perm( 7000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 7000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-1_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 5000, '2100-1_coalplants-1_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 5000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 7000, '2100-1_coalplants-1_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-1');
-    await perm( 5000, '2100-1_coalplants-1_industry-1_roadtraffic-1_15degrees-1_population-0_wildfires-0_sealevel-0_wbgt-1');
-    
-    await perm( 5000, '2100-0_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-0_wildfires-0_sealevel-0_wbgt-0');
 
     await perm( 7000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-1_sealevel-0_wbgt-0');
-    await perm( 5000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-1_wbgt-0');
-    await perm( 5000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-0_wbgt-1');
+    await perm(10000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-1_wbgt-0');
+    await perm(10000, '2100-1_coalplants-0_industry-0_roadtraffic-0_15degrees-0_population-1_wildfires-0_sealevel-0_wbgt-1');
+    await defer(5000);
+    document.getElementById('link').style.opacity = '1.0';
 }
 
 mapManager.loadMappings();
