9c3f9d117093a9fc1fdf354c7405e1ff60adc602
Traines <git@traines.eu>
2023-10-08 20:34:49 +0000

    styling, mannheim

--- 010-deutschlandtakt/deutschlandtakt.js
+++ 010-deutschlandtakt/deutschlandtakt.js
@@ -12,26 +12,32 @@ fetch('research/analysis-dtakt-railml.json').then(resp => resp.json()).then(json
     stations = json['stations'];
     console.log(json['stations']['AELB']);
     const container = document.getElementById('regionalverkehr');
-    console.log(Object.keys(stations));
+    console.log(Object.keys(stations).length);
+    const bb = new TNA.BoundingBox(TNA.Vector.NULL, TNA.Vector.NULL);
     for(let key of Object.keys(stations)) {
         const el = document.createElementNS(SVGNS, 'rect');
         el.dataset.lonLat = stations[key]['lon'] + ' ' + stations[key]['lat'];
+        bb.add(new TNA.Vector(stations[key]['lon'], stations[key]['lat']));
         el.dataset.station = 'rv_'+key;
-        el.dataset.from = "2050 1";
+        //el.dataset.from = "2050 1";
         const m = Math.max(stations[key]['dtakt'], stations[key]['gtfs']);
         const diff = stations[key]['gtfs'] == 0 ? 0 : m-stations[key]['gtfs'];
         el.className.baseVal = 'station dtakt-'+fuzzy(m)+' diff-'+fuzzy(diff) + ' ' + stations[key]['dtakt'] + ' ' + stations[key]['gtfs'];
         container.appendChild(el);
     }
+    console.log(bb);
 
-
-    TNA.Config.default.gravitatorUseInclinationInertness = true;
+    TNA.Config.default.gravitatorUseInclinationInertness = false;
     TNA.Config.default.gravitatorInertness = 0.25;
     TNA.Config.default.mapProjectionScale = 100;
     TNA.Config.default.trainWagonLength = 10;
     TNA.Config.default.trainTimetableSpeed = 180;
-    var event = new Event('startTransportNetworkAnimator');
-    document.dispatchEvent(event);
+
+    window.setTimeout(function() {    
+      var event = new Event('startTransportNetworkAnimator');
+      document.dispatchEvent(event);
+    }, 1);
+
 })
 
 let intvl;
